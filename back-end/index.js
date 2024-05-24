dc10bd38ac4401bc720864fbbba9ee3a21c87a64
Shohjahon-n <pazvat56@gmail.com>
2024-05-24 16:44:35 +0500

    fixed and also

--- back-end/index.js
+++ back-end/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+
 const app = express();
 const port = process.env.PORT || 1111;
-const bodyParser = require('body-parser');
+
 app.use(cors());
 app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, '../client/build')));
 
 let data = [
     {
@@ -494,7 +499,6 @@ let data = [
         "isTrending": false
     }
 ]
-
 app.get('/data', (req, res) => {
     res.send(data);
 });
@@ -510,6 +514,10 @@ app.patch('/data/:id', (req, res) => {
     } else {
         res.status(404).send({ message: 'Item not found' });
     }
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
 app.listen(port, () => {
